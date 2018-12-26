7a342955b97cd039b67b84f1e0358113bfb35adb
mayoneko <ev60622@meiji.ac.jp>
2018-12-26 19:56:09 +0900

    feat(kt): add Dealer Class

---
+++ src/main/kotlin/hearts/Dealer.kt
@@ -0,0 +1,63 @@
+package hearts
+
+import kotlin.random.Random
+
+class Dealer {
+
+    val playerNum = 4
+
+    // key: rankID
+    // val: playerID
+    private val playerRanking = mutableMapOf<Int, Int>()
+
+    var turnPlayerID: Int = 0
+
+
+    //game manage functions
+
+    fun dealCardsToPlayers(board: Board, players: List<Player>) {
+        val randomInt = Random.nextInt(playerNum)
+        for ((cardID, playerID) in (0..51).shuffled().zip((0..51).map { (it + randomInt) % playerNum })) {
+            board.setCardToHand(cardID, playerID)
+        }
+        for (player in players) {
+            player.cards = board.getHand(player.id).map { cardID -> Card(cardID) }
+        }
+    }
+
+    fun setStartPlayer(players: List<Player>) {
+        for (player in players) {
+            if (player.cards.any { card -> card.suit == Card.CLUBS && card.number == 2 }) {
+                this.turnPlayerID = player.id
+                return
+            }
+        }
+    }
+
+    fun handleExchange(board: Board, players: List<Player>) {
+        //TODO
+
+    }
+
+    fun playTurn(board: Board, player: Player, otherPlayersStatus: List<Player.Status>) {
+        //TODO
+    }
+
+    fun handleTurn(players: List<Player>) {
+        turnPlayerID = (turnPlayerID + 1) % playerNum
+    }
+
+    fun isGameEnded(players: List<Player>): Boolean {
+        return players.all { player ->
+            player.Status().handNum == 0
+        }
+    }
+
+    fun getPlayerRanking(): Map<Int, Int> {
+//        println("GameSet\n")
+//        for (rankID in 1..playerNum) {
+//            println("${rankID}位 : Player${playerRanking[rankID]}")
+//        }
+        return playerRanking
+    }
+}
