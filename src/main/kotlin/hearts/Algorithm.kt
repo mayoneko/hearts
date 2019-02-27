8bd3efbb55db0928686b6def4cb64d77c1b58d5d
mayoneko <ev60622@meiji.ac.jp>
2019-02-27 18:42:15 +0900

    fix(kt): fix getPlayableHand functions

--- src/main/kotlin/hearts/Algorithm.kt
+++ src/main/kotlin/hearts/Algorithm.kt
@@ -18,9 +18,26 @@ abstract class Algorithm {
         @JvmStatic
         fun getPlayableHand(board: List<Card>, hand: List<Card>, leadSuit: Int?, isHeartBroken: Boolean): List<Card> {
             val playableCards = mutableListOf<Card>()
-            //TODO
+            if (leadSuit == null) {
+                if (isHeartBroken) {
+                    playableCards.addAll(hand)
+                } else {
+                    playableCards.addAll(hand.filter { card ->
+                        card.suit != Card.HEARTS
+                    })
+                    if(playableCards.isEmpty()){
+                        playableCards.addAll(hand)
+                    }
+                }
+            }else{
+                playableCards.addAll(hand.filter { card ->
+                    card.suit != leadSuit
+                })
+                if(playableCards.isEmpty()){
+                    playableCards.addAll(hand)
+                }
+            }
             return playableCards
         }
-
     }
 }
