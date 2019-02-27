92be48e8d154790f3bc058912a8000eb83b62652
mayoneko <ev60622@meiji.ac.jp>
2019-02-27 18:33:46 +0900

    fix(kt): fix Card.strength

--- src/main/kotlin/hearts/Card.kt
+++ src/main/kotlin/hearts/Card.kt
@@ -1,6 +1,6 @@
 package hearts
 
-data class Card(val id: Int, val suit: Int, val number: Int, val strength: Int) {
+data class Card(val id: Int, val suit: Int, val number: Int) {
     companion object {
         const val HEARTS = 0
         const val DIAMONDS = 1
@@ -8,13 +8,13 @@ data class Card(val id: Int, val suit: Int, val number: Int, val strength: Int) 
         const val SPADES = 3
     }
 
-    constructor(_cardID: Int) : this(_cardID, _cardID / 13, _cardID % 13 + 1, if (_cardID==1) 14 else _cardID % 13 + 1) {
+    constructor(_cardID: Int) : this(_cardID, _cardID / 13, _cardID % 13 + 1) {
         if (_cardID !in (0..51)) {
             throw IllegalArgumentException("cardID argument must be in 0..51")
         }
     }
 
-    constructor(_suit: Int, _number: Int) : this(_suit * 13 + (_number - 1), _suit, _number, if (_number==1) 14 else _number) {
+    constructor(_suit: Int, _number: Int) : this(_suit * 13 + (_number - 1), _suit, _number) {
         if (_suit !in (0..3)) {
             throw IllegalArgumentException("suit argument must be in 0..3")
         }
@@ -28,6 +28,8 @@ data class Card(val id: Int, val suit: Int, val number: Int, val strength: Int) 
         this.suit == SPADES && this.number == 12 -> 13
         else -> 0
     }
+
+    val strength: Int = if (number==1) 14 else number
 
     override fun toString(): String {
         val suitStr = when (this.suit) {
