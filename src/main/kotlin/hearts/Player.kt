10ba49918f6d098f9a1cba8ed1fdebad497ea914
mayoneko <ev60622@meiji.ac.jp>
2019-02-27 17:02:01 +0900

    fix(kt): add playerID to board cards

--- src/main/kotlin/hearts/Player.kt
+++ src/main/kotlin/hearts/Player.kt
@@ -5,15 +5,20 @@ class Player(val id: Int, private val algorithm: Algorithm) {
     inner class Status {
         val id = this@Player.id
         val handNum = this@Player.cards.size
-        val chosenCard:List<Card> = this@Player.chosenCardList
+        val chosenCard: List<Card> = this@Player.chosenCardList
     }
 
     var cards = listOf<Card>()
 
     private var chosenCardList = mutableListOf<Card>()
 
-    fun playCard(board: List<Card>, hand: List<Card>, otherPlayersStatus: List<Player.Status>,isHeartBroken:Boolean): Card {
-        val chosenCard = algorithm.choiceCard(board, hand, otherPlayersStatus, isHeartBroken)
+    fun playCard(
+        boardMap: List<Pair<Card, Int>>,
+        hand: List<Card>,
+        otherPlayersStatus: List<Player.Status>,
+        isHeartBroken: Boolean
+    ): Card {
+        val chosenCard = algorithm.choiceCard(boardMap, hand, otherPlayersStatus, isHeartBroken)
         chosenCardList.add(chosenCard)
         return chosenCard
     }
