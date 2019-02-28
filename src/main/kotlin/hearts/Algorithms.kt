edcfb823d0cce8c823afe2bb4edeb19c40c6e96b
mayoneko <ev60622@meiji.ac.jp>
2019-02-28 17:09:57 +0900

    fix(kt): add in order to debug

--- src/main/kotlin/hearts/Algorithms.kt
+++ src/main/kotlin/hearts/Algorithms.kt
@@ -10,6 +10,37 @@ class RandomAlgorithm : Algorithm() {
         leadSuit: Int?,
         isHeartBroken: Boolean
     ): Card {
-        return getPlayableHand(hand,leadSuit,isHeartBroken).random()
+        return getPlayableHand(hand, leadSuit, isHeartBroken).random()
+    }
+}
+
+class TestAlgorithm : Algorithm() {
+    override fun choiceCard(
+        boardMap: List<Pair<Card, Int>>,
+        hand: List<Card>,
+        otherPlayersStatus: List<Player.Status>,
+        leadSuit: Int?,
+        isHeartBroken: Boolean
+    ): Card {
+        val playable = getPlayableHand(hand, leadSuit, isHeartBroken)
+        if (leadSuit == null) {
+            val suitList = mutableListOf(0, 0, 0, 0)
+            playable.forEach {
+                suitList[it.suit]++
+            }
+            return playable.filter {
+                it.suit == suitList.max()
+            }.minBy {
+                it.strength
+            } ?: playable.random()
+        }else{
+            return playable.filter {
+                it.suit == leadSuit
+            }.minBy {
+                it.strength
+            } ?: playable.maxBy {
+                it.point * 5 + it.strength
+            } ?: playable.random()
+        }
     }
 }
--- src/main/kotlin/hearts/hearts.kt
+++ src/main/kotlin/hearts/hearts.kt
@@ -1,5 +1,11 @@
 package hearts
 
 fun main(args: Array<String>) {
-    println(Game(listOf(RandomAlgorithm(), RandomAlgorithm(), RandomAlgorithm(), RandomAlgorithm())).run())
+    val result = mutableListOf(0, 0, 0, 0)
+    for (i in 0 until 100) {
+        Game(listOf(TestAlgorithm(), RandomAlgorithm(), RandomAlgorithm(), RandomAlgorithm())).run().forEach{
+            result[it.key] += it.value
+        }
+    }
+    println(result)
 }
